727d0d0f4d32d7ad7fcf767948101f855907c6ab
AntonioErat <100047629+AntonioErat@users.noreply.github.com>
2022-03-20 12:46:35 -0400

    Updating Health & Lives

--- Adventure/Player.py
+++ Adventure/Player.py
@@ -3,6 +3,8 @@ class player:
         self.name = "Adventurer"
         self.livesLeft = 3
         self.boulderVisits = 0
+        self.maxHealth = 100
+        self.health = self.maxHealth
         
     def getName(self):
         return self.name
@@ -22,3 +24,29 @@ class player:
 
     def visitBoulder(self):
         self.boulderVisits += 1
+    
+    def addLife(self, lives):
+        self.livesLeft += lives
+        self.health = self.maxHealth
+
+    def loseLife(self, lives=1):
+        self.livesLeft -= lives
+        if self.livesLeft < 0:
+            self.livesLeft = 0
+        if self.livesLeft == 0:
+            self.health = 0
+        elif self.livesLeft >= 1:
+            self.health = self.maxHealth
+
+    def getHealth(self):
+        return self.health
+    
+    def addHealth(self, health):
+        self.health += health
+        if self.health > self.maxHealth:
+            self.health=self.maxHealth
+
+    def loseHealth(self, health):
+        self.health -= health
+        if self.health < 0:
+            self.loseLife()
